eb1040f7dd6d7a20f43564130de2f63e058c7670
osdnk <micosa97@gmail.com>
2026-01-13 11:43:05 +0200

    comment

--- src/protocol/sumchecks/helpers.rs
+++ src/protocol/sumchecks/helpers.rs
@@ -376,6 +376,7 @@ pub(crate) fn split_projection_flatter(
 /// The result is then used in the LS-variable linear sumcheck component, which gets
 /// multiplied with the elder-variable component to form the complete projection
 /// coefficient sumcheck.
+/// // TODO: this can be computed fast as projection_flatter_1: &PreprocessedRow. is over fields
 pub(crate) fn projection_flatter_1_times_matrix(
     projection_matrix: &ProjectionMatrix,
     projection_flatter_1: &PreprocessedRow,
