5a501372db07205b0b3320b61b85bad2e143fd9f
Prashanth Ramakrishna <praz.ramakrishna@gmail.com>
2026-04-25 10:20:07 -0400

    clean module ordering

--- src/protocol/sumcheck_utils/mod.rs
+++ src/protocol/sumcheck_utils/mod.rs
@@ -6,11 +6,15 @@ pub mod hypercube_point;
 pub mod linear;
 pub mod polynomial;
 pub mod product;
-#[cfg(feature = "profile-sumcheck")]
-pub mod profile;
 pub mod ring_to_field_combiner;
 pub mod selector_eq;
 pub mod sum;
+
+// some basic instrumentation, to be deprecated on 
+// arrival of complete profiling infrastructure
+#[cfg(feature = "profile-sumcheck")]
+pub mod profile;
+
 
 #[cfg(test)]
 mod tests {
