a304b5a4d09d3307e24204c493c79f9a7635b9ea
Michał Osadnik <micosa97@gmail.com>
2026-09-07 23:44:40 +0300

    Parallelise the CRT commitment behind `parallel` (#114)

--- benches/bench.rs
+++ benches/bench.rs
@@ -59,7 +59,10 @@ fn bench_decompose(c: &mut Criterion) {
     group.bench_function("base 2^16, radix 2", |bencher| {
         bencher.iter(|| black_box(decompose(black_box(&input), 16, 2)));
 fn bench_commitment(c: &mut Criterion) {
+    #[cfg(not(feature = "parallel"))]
     use rokoko::protocol::commitment::commit_basic;
+    #[cfg(feature = "parallel")]
+    use rokoko::protocol::commitment::commit_basic_parallel as commit_basic;
     use rokoko::protocol::commitment_crt::{commit_basic_crt, digits_l2, CrtKey, Plan};
     use rokoko::protocol::crs::CRS;
 
